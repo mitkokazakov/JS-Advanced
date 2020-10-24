4873a8cc75036ce48215a990fe43fe35b86a3029
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-10-24 10:26:27 +0300

    Another task added

---
+++ DOM Manipulations/02. Notification/scripts.js
@@ -0,0 +1,12 @@
+function notify(message) {
+    
+    const notificationDiv = document.querySelector('#notification');
+
+    notificationDiv.textContent = message;
+
+    notificationDiv.style.display = 'block';
+
+    setTimeout(() => {
+        notificationDiv.style.display = 'none'
+    }, 2000);
+}
